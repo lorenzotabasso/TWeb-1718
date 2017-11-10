29df66c598b7e157c52d6e74e1569bd1f7384d02
Lorenzo Tabasso <st159210@educ.di.unito.it>
2017-11-10 14:29:57 +0100

    Added Form in signup.php

--- lab04/signup.php
+++ lab04/signup.php
@@ -8,7 +8,58 @@
  * Descrizione: una pagina contenente un form che l'utente può compilare per richiedere la registrazione di un nuovo account.
  * Contenuto:
  */
-echo("CIAOOO");
 ?>
+
+<form action="signup-submit.php">
+    <fieldset>
+        <ul>
+            <li>
+                <label>
+                    Name: <input type="text" size="10" maxlength="8">
+                </label>
+            </li>
+            <li>
+                <label>
+                    Gender:
+                    <input type="radio" name="gender" value="Male"> Male
+                    <input type="radio" name="gender" value="Female" checked="checked"> Female
+                </label>
+            </li>
+            <li>
+                <label>
+                    Age: <input type="text" size="6" maxlength="2">
+                </label>
+            </li>
+            <li>
+                <label>
+                    Personality Type: <input type="text" size="6" maxlength="4">
+                    (<a href="http://www.humanmetrics.com/cgi-win/JTypes2.asp">Don't know your type?</a>)
+                </label>
+            </li>
+            <li>
+                <label>
+                    Favorite OS:
+                    <select name="favoriteOS">
+                        <option>Windows</option>
+                        <option>Mac OS X</option>
+                        <option selected="selected">Linux</option>
+                    </select>
+                </label>
+            </li>
+            <li>
+                <label>
+                    Seeking Age:
+                    <textarea rows="1" cols="6" maxlength="2"> Age </textarea> to
+                    <textarea rows="1" cols="6" maxlength="2"> Age </textarea>
+                </label>
+            </li>
+            <li>
+                <label>
+                    <input type="submit" value="Sign Up">
+                </label>
+            </li>
+        </ul>
+    </fieldset>
+</form>
 
 <?php include("bottom.html"); ?>
